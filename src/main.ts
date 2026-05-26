0d634360e24f3f558dbe4372c08416cb4f241743
Daniel11Doo <daniels4anchez@gmail.com>
2026-05-26 12:08:06 -0600

    feat(swagger): add Swagger documentation to auth and products

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -15,6 +16,16 @@ async function bootstrap() {
   );
 
   app.setGlobalPrefix('api');
+
+  const config = new DocumentBuilder()
+    .setTitle('POS API')
+    .setDescription('API del sistema de punto de venta')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
 
   await app.listen(process.env.PORT ?? 3000);
 }
